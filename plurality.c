8990130201a22ec57f48a961f2ee94d011b83595
Heintz Boerje <heintzrboerje@protonmail.com>
2022-04-06 12:50:22 -0400

    Finished plurality.c

--- plurality.c
+++ plurality.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 typedef struct{
     char* name;
@@ -16,21 +17,40 @@ int main(int argc, char* argv[]){
         printf("Usage: ./plurality candidate_names\n");
         return 1;
     }
+
     candidate candidates[argc-1];
+
     for(int i = 1; i < argc; i++){
         candidates[i-1].name = argv[i];
         candidates[i-1].votes = 0;
     }
-    printf("%s, %s\n", candidates[0].name, candidates[1].name);
+
     int voters = get_int("Number of voters: ");
-    int i = argc-1;
-    
-    while(voters > 0){
-        char* vote = get_string("Vote: ");
-        while(i > 0){
-            printf("%s\n", candidates[i].name);
-            i--;
+
+    while(voters>0){
+        char *vote = get_string("Vote: ");
+        for(int i = 0; i < (argc-1); i++){
+            if(strcmp(vote, candidates[i].name)==0){
+                candidates[i].votes++;
+            }
         }
         voters--;
     }
+
+    for(int i = 0; i < argc-1; i++){
+        for(int j=i+1; j < argc-1; j++){
+            if(candidates[j].votes > candidates[i].votes){
+                candidate temp = candidates[i];
+                candidates[i] = candidates[j];
+                candidates[j] = temp;
+            }
+        }
+    }
+
+    int i = 0;
+    while(candidates[i].votes == candidates[0].votes){
+        printf("%s ", candidates[i].name);
+        i++;
+    }
+    printf("\n");
 }
